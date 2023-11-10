edb2ef04e59f386208bf4c2bb80a736772572afa
Kireakire <conforti.erika@gmail.com>
2023-11-10 12:14:57 +0100

    Capteurs_Courant

--- NUCLEO-G474RET6-Inverter_Pinout/Core/Src/adc.c
+++ NUCLEO-G474RET6-Inverter_Pinout/Core/Src/adc.c
@@ -75,7 +75,7 @@ void MX_ADC1_Init(void)
 
   /** Configure Regular Channel
   */
-  sConfig.Channel = ADC_CHANNEL_8;
+  sConfig.Channel = ADC_CHANNEL_12;
   sConfig.Rank = ADC_REGULAR_RANK_1;
   sConfig.SamplingTime = ADC_SAMPLETIME_2CYCLES_5;
   sConfig.SingleDiff = ADC_SINGLE_ENDED;
--- NUCLEO-G474RET6-Inverter_Pinout/Core/Src/capteurs.c
+++ NUCLEO-G474RET6-Inverter_Pinout/Core/Src/capteurs.c
@@ -9,13 +9,25 @@
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
+#include "stdio.h"
+#include "adc.h"
+
+float digit_Max=4095;
+float plage_Dynamique=3300; //mV
+float gain=50; // mV/A
+float offset_Voltage=5; //mV
 
 
 
 void  mesure_Current_U(){
-//	uint32_t valeur;
-//	HAL_ADC_Start(&hadc1);
-//	HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
-//	valeur = HAL_ADC_Get_Value(&hadc1);
-
+	float buffer[32];
+	float measured_Value;
+	float courant;
+	HAL_ADC_Start(&hadc1);
+	HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
+	measured_Value = HAL_ADC_GetValue(&hadc1);
+	courant=1/gain*((plage_Dynamique*measured_Value)/digit_Max-offset_Voltage);
+	HAL_ADC_Stop(&hadc1);
+	HAL_UART_Transmit(&huart2,(void*)buffer, sprintf((char*)buffer, "%f \r\n", (float) courant),10);
+	HAL_Delay(1000);
 }
